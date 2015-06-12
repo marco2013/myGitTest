c7d5df15ef008701e84d87c4037e7b8fcc0712a3
zwl <zwl@zwltekiMac-mini.local>
2015-06-12 22:21:57 +0800

    Initial Commit

---
+++ myGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myGitTest
+//
+//  Created by zwl on 15/6/12.
+//  Copyright (c) 2015年 zwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

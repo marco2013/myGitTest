bc059cbd1c61e366344c04d7640d51a71118de45
zwl <zwl@zwltekiMac-mini.local>
2015-06-13 00:02:01 +0800

    comment new files and Project Settings

---
+++ myGitTest/xxxxxInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  xxxxxInterfaceController.h
+//  myGitTest
+//
+//  Created by zwl on 15/6/12.
+//  Copyright (c) 2015年 zwl. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface xxxxxInterfaceController : WKInterfaceController
+
+@end
---
+++ myGitTest/xxxxxInterfaceController.m
@@ -0,0 +1,36 @@
+//
+//  xxxxxInterfaceController.m
+//  myGitTest
+//
+//  Created by zwl on 15/6/12.
+//  Copyright (c) 2015年 zwl. All rights reserved.
+//
+
+#import "xxxxxInterfaceController.h"
+
+@interface xxxxxInterfaceController ()
+
+@end
+
+@implementation xxxxxInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
